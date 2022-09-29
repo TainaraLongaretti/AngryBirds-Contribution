fd38e2f9db1a00ad084681c7100c0ff820c7ea9f
TainaraLongaretti <106778351+TainaraLongaretti@users.noreply.github.com>
2022-09-29 18:21:20 -0300

    Update Pig.js

--- Pig.js
+++ Pig.js
@@ -9,7 +9,7 @@ class Pig extends BaseClass {
   //checar a velocidade do pig no console
   // diminuir o valor da condição para ser mais facil de derrota-lo
    console.log("pig velocity= " + this.body.speed);
-   if(this.body.speed < 7){
+   if(this.body.speed < 3){
 
     super.display();
    }
@@ -18,7 +18,7 @@ class Pig extends BaseClass {
      World.remove(world, this.body);
      push();
      //aumentar o valor de decremente da visibilidade para o porco sumir mais rápido.
-     this.Visiblity = this.Visiblity - 1;
+     this.Visiblity = this.Visiblity - 3;
 
      tint(255,this.Visiblity);
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
